2b18c09f03088ca80556144dc202a9d44153df78
Mike Grudic <mike.grudic@gmail.com>
2026-03-02 18:03:29 -0500

    test: test to verify consistency between meshoid slice and coordinate grids

--- src/vizmo/__init__.py
+++ src/vizmo/__init__.py
@@ -1,1 +1,3 @@
 """Routines for simulation data visualization with e.g. matplotlib"""
+
+from .coordinate_grid import coordinate_grid
--- test/test_slice.py
+++ test/test_slice.py
@@ -1,5 +1,19 @@
-import pytest
+from vizmo import coordinate_grid
+from meshoid import Meshoid, particle_glass
+import numpy as np
 
 
 def test_slice():
-    assert True
+    """Tests that a slice reconstructs the coordinate functions exactly,
+    and consistency between the coordinate grid function and the meshoid slice grid"""
+    x = particle_glass(16**3)
+    sliceargs = {"res": 128, "size": 1.0, "center": 0.5 * np.ones(3)}
+    X, Y = coordinate_grid(**sliceargs)
+    xslice = Meshoid(x).Slice(x[:, 0], **sliceargs, order=2)
+    assert np.all(np.isclose(X, xslice, atol=1e-15))
+    xslice = Meshoid(x).Slice(x[:, 1], **sliceargs, order=2)
+    assert np.all(np.isclose(Y, xslice, atol=1e-15))
+    xslice = Meshoid(x).Slice(x[:, 0], **sliceargs, order=1)
+    assert np.all(np.isclose(X, xslice, atol=1e-15))
+    xslice = Meshoid(x).Slice(x[:, 1], **sliceargs, order=1)
+    assert np.all(np.isclose(Y, xslice, atol=1e-15))
